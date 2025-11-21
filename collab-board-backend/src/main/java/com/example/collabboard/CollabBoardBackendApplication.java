b48f22f8ab3ec3da0df3a3c95f26491a25c2d898
avinash-rao <avinash_it@yahoo.com>
2025-11-21 18:22:46 +0530

    initial commit

---
+++ collab-board-backend/src/main/java/com/example/collabboard/CollabBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.collabboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabBoardBackendApplication.class, args);
+	}
+
+}
---
+++ collab-board-backend/src/test/java/com/example/collabboard/CollabBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collabboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
